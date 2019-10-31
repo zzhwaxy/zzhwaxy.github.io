d07a8ad50ce9da29fdf964c8cbfc713b65e46db9
zzhwaxy <1660611285@qq.com>
2019-10-31 14:21:22 +0800

    adjust size

--- assets/waifu-tips.js
+++ assets/waifu-tips.js
@@ -52,11 +52,11 @@ live2d_settings['showCopyMessage']      = true;         // 显示 复制内容 �
 live2d_settings['showWelcomeMessage']   = true;         // 显示进入面页欢迎词
 
 //看板娘样式设置
-live2d_settings['waifuSize']            = '280x250';    // 看板娘大小，例如 '280x250', '600x535'
-live2d_settings['waifuTipsSize']        = '250x70';     // 提示框大小，例如 '250x70', '570x150'
-live2d_settings['waifuFontSize']        = '12px';       // 提示框字体，例如 '12px', '30px'
-live2d_settings['waifuToolFont']        = '14px';       // 工具栏字体，例如 '14px', '36px'
-live2d_settings['waifuToolLine']        = '20px';       // 工具栏行高，例如 '20px', '36px'
+live2d_settings['waifuSize']            = '336x300';    // 看板娘大小，例如 '280x250', '600x535'
+live2d_settings['waifuTipsSize']        = '300x84';     // 提示框大小，例如 '250x70', '570x150'
+live2d_settings['waifuFontSize']        = '15px';       // 提示框字体，例如 '12px', '30px'
+live2d_settings['waifuToolFont']        = '15px';       // 工具栏字体，例如 '14px', '36px'
+live2d_settings['waifuToolLine']        = '33px';       // 工具栏行高，例如 '20px', '36px'
 live2d_settings['waifuToolTop']         = '0px'         // 工具栏顶部边距，例如 '0px', '-60px'
 live2d_settings['waifuMinWidth']        = '768px';      // 面页小于 指定宽度 隐藏看板娘，例如 'disable'(禁用), '768px'
 live2d_settings['waifuEdgeSide']        = 'left:0';     // 看板娘贴边方向，例如 'left:0'(靠左 0px), 'right:30'(靠右 30px)
@@ -67,7 +67,7 @@ live2d_settings['waifuDraggableRevert'] = true;         // 松开鼠标还原拖
 live2d_settings['l2dVersion']           = '1.4.2';        // 当前版本
 live2d_settings['l2dVerDate']           = '2018.11.12'; // 版本更新日期
 live2d_settings['homePageUrl']          = 'auto';       // 主页地址，可选 'auto'(自动), '{URL 网址}'
-live2d_settings['aboutPageUrl']         = 'https://www.fghrsh.net/post/123.html';   // 关于页地址, '{URL 网址}'
+live2d_settings['aboutPageUrl']         = 'https://github.com/zzhwaxy';   // 关于页地址, '{URL 网址}'
 live2d_settings['screenshotCaptureName']= 'live2d.png'; // 看板娘截图文件名，例如 'live2d.png'
 
 /****************************************************************************************************/
